6521ee6ac3676a2f2e3f37c2da30891d1ffb542b
Danila <idanila331@gmail.com>
2026-09-04 19:05:24 +0300

    returned the base_button to its previous state

--- src/shared/ui/BaseButton/BaseButton.jsx
+++ src/shared/ui/BaseButton/BaseButton.jsx
@@ -1,20 +1,24 @@
 import { NavLink } from 'react-router';
 import { Button } from '@mui/material';
 
+import styles from './baseButton.module.css';
+
 function BaseButton({ text, address }) {
   return (
-    <NavLink to={address}>
-      <Button
-        variant="contained"
-        sx={{
-          textTransform: 'none',
-          fontSize: '1.2em',
-          '&:hover': { color: 'secondary.contrastText' },
-        }}
-      >
-        {text}
-      </Button>
-    </NavLink>
+    <div className={styles.link}>
+      <NavLink to={address}>
+        <Button
+          variant="contained"
+          sx={{
+            textTransform: 'none',
+            fontSize: '1.2em',
+            '&:hover': { color: 'secondary.contrastText' },
+          }}
+        >
+          {text}
+        </Button>
+      </NavLink>
+    </div>
   );
 }
 
